7beb36330fa6dbe54f39ad13673af12ed192201e
Vinay Saddanapu <60392514+vinaysaddanapu9@users.noreply.github.com>
2026-06-25 12:15:52 +0530

    Modified the window size

--- src/main.py
+++ src/main.py
@@ -170,7 +170,7 @@ style.theme_use("clam")
 
 root.title("GoldPOS v0.1.5")
 root.iconbitmap(icon_path)
-root.geometry("580x720")
+root.geometry("520x680")
 
 root.configure(bg="#f8f5ef")
 root.resizable(False, False)
